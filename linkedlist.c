eb81e23e0117761322187a279e8ce73980c3c347
Baron Leonardo <mohamedayman.fcis@zoho.com>
2016-10-25 10:44:45 +0200

    constrcut most of LinkedList -insert, clean, size, ...-

--- linkedlist.c
+++ linkedlist.c
@@ -15,3 +15,148 @@
  */
 
 #include "linkedlist.h"
+#include <stdint.h>
+#include <string.h>
+
+struct LinkedList_Node {
+	LinkedList_Node* next;
+	LinkedList_Node* prev;
+	void* data;
+};
+
+struct LinkedList_list {
+	uint32_t length;
+	LinkedList_Node *head;      /* List head, dummy node. */
+    LinkedList_Node *tail;      /* List tail, dummy node. */
+};
+
+LinkedList_list* LinkedList_new()
+{
+	LinkedList_list* list = malloc( sizeof(LinkedList_list) );
+	list->length = 0;
+	list->head = NULL;
+	list->tail = NULL;
+
+	return list;
+}
+
+// #include <stdio.h>
+
+void* LinkedList_push_back( LinkedList_list* list, void* data, size_t size, bool data_local )
+{
+	LinkedList_Node* node = malloc(sizeof(LinkedList_Node) + size);
+	// printf("%lu\n", sizeof(LinkedList_Node) + size);
+	// printf("%lu\n", sizeof(2 * sizeof(LinkedList_list *)) + size);
+	// printf("%p %p %p %p\n", &node->next, &node->prev, ((void*)node + 2 * sizeof(void*)), &node->data);
+
+	if( node == NULL )
+		return NULL;
+
+	if( data_local == true )
+	{
+		// this point to the address should be used for node's data 
+		// node->data = node->prev + sizeof(void*);
+		node->data = malloc(size);
+		memcpy(node->data, data, size);
+	}
+
+	else
+		node->data = data;
+
+	if( list->length != 0 )
+	{
+		list->tail->next = node;
+		node->prev = list->tail;
+	}
+
+	else
+	{
+		list->head = node;
+		node->prev = NULL;
+	}
+
+	list->tail = node;
+	node->next = NULL;
+	list->length++;
+
+	return node;
+}
+
+// TODO:
+void* LinkedList_push_front( LinkedList_list* list, void* data, size_t size, bool is_data_local )
+{
+
+}
+
+// TODO:
+void* LinkedList_pop_back( LinkedList_list* list )
+{
+
+}
+
+// TODO:
+void* LinkedList_pop_front( LinkedList_list* list )
+{
+
+}
+
+void* LinkedList_get_data(LinkedList_Node *node)
+{
+	return node->data;
+}
+
+void* LinkedList_get_begin(LinkedList_list* list)
+{
+	return list->head;
+}
+
+void* LinkedList_get_last(LinkedList_list* list)
+{
+	return list->tail;
+}
+
+size_t LinkedList_get_length( LinkedList_list *list )
+{
+	return list->length;
+}
+
+void* LinkedList_prev(LinkedList_Node* node)
+{
+	return node->prev;
+}
+
+void* LinkedList_next(LinkedList_Node* node)
+{
+	return node->next;
+}
+
+// TODO:
+bool LinkedList_remove(LinkedList_Node* node)
+{
+	return true;
+}
+
+// TODO
+bool LinkedList_remove_from(LinkedList_Node* from, LinkedList_Node* to)
+{
+	return true;
+}
+
+void LinkedList_clean(LinkedList_list *list)
+{
+	LinkedList_Node *current_node = list->head;
+	LinkedList_Node *next_node = list->head;
+
+	while((current_node = next_node) != NULL)
+	{
+		next_node = next_node->next;
+		free(current_node->data);
+		free(current_node);
+	}
+
+	list->length = 0;
+	list->head = NULL;
+	list->tail = NULL;
+
+	free(list);
+}
--- linkedlist.h
+++ linkedlist.h
@@ -19,4 +19,26 @@
 #ifndef LINKEDLIST_H_
 #define LINKEDLIST_H_
 
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct LinkedList_Node LinkedList_Node;
+typedef struct LinkedList_list LinkedList_list;
+
+LinkedList_list* LinkedList_new();
+
+void* LinkedList_push_back( LinkedList_list* list, void* data, size_t size, bool is_data_local );
+void* LinkedList_push_front( LinkedList_list* list, void* data, size_t size, bool is_data_local );
+void* LinkedList_pop_back( LinkedList_list* list );
+void* LinkedList_pop_front( LinkedList_list* list );
+size_t LinkedList_get_length( LinkedList_list *list );
+void* LinkedList_get_data(LinkedList_Node *node);
+void* LinkedList_get_begin(LinkedList_list* list);
+void* LinkedList_get_last(LinkedList_list* list);
+void* LinkedList_next(LinkedList_Node* node);
+void* LinkedList_prev(LinkedList_Node* node);
+bool LinkedList_remove(LinkedList_Node* node);
+bool LinkedList_remove_from(LinkedList_Node* from, LinkedList_Node* to);
+void LinkedList_clean(LinkedList_list *list);
+
 #endif /*LINKEDLIST_H_*/
