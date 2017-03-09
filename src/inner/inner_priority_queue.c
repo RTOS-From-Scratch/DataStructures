93db73f8b57d47cc437fa3bac5dda91e0fcadd57
sara <saraadel.fcis2012@gmail.com>
2017-03-09 14:22:51 +0200

    create directory called inner include 2 files called inner_priority_queue.* that hold the abstracted functions used by the priority_queue and inverted_priority_queue to be hidden from the user and be easier in use by the 2 classes

---
+++ src/inner/inner_priority_queue.c
@@ -0,0 +1,93 @@
+#include "inner_priority_queue.h"
+
+void __exchange_node(__PQueue_Node * node1, __PQueue_Node * node2 )
+{
+    // exchange data
+    long tmp = (long)node1->data;
+    node1->data = node2->data;
+    node2->data = (void *)tmp;
+
+    // exchange priority
+    tmp = node1->priority;
+    node1->priority = node2->priority;
+    node2->priority = (int8_t)tmp;
+}
+
+void __sink_LastElement( __PQueue *p_queue )
+{
+    __PQueue_Node* queue = p_queue->queue;
+    int current_parent = 0;
+    int current_child  = 1;
+
+    while(current_child < p_queue->curr_index)
+    {
+        // check if the right child exists
+        // check if the right child has higher priority
+        if(current_child + 1 <= p_queue->curr_index &&
+           queue[current_child].priority < queue[current_child + 1].priority)
+            current_child++;
+
+        __exchange_node(&queue[current_parent], &queue[current_child]);
+
+        current_parent = current_child;
+        current_child  = current_parent * 2 + 1;
+    }
+}
+
+void __swim_LastElement( __PQueue *p_queue )
+{
+    // 1- child -> last node
+    // 2- cmp child, parent where parent -> (child-1)/2
+    // 3- child > parent -> swap and child_index = parent_index
+    // 4- else get out
+    int8_t current_index = p_queue->curr_index;
+    int8_t parent_index;
+    __PQueue_Node* queue = p_queue->queue;
+
+    while (current_index > 0 &&
+           queue[parent_index =  (current_index - 1) / 2].priority < queue[current_index].priority)
+    {
+        __exchange_node(&queue[parent_index], &queue[current_index]);
+        current_index = parent_index;
+    }
+}
+
+void __sink_LastElement_inverted( __PQueue *p_queue )
+{
+    __PQueue_Node* queue = p_queue->queue;
+    int current_parent = 0;
+    int current_child  = 1;
+
+    while(current_child < p_queue->curr_index)
+    {
+        // check if the right child exists
+        // check if the right child has higher priority
+        if(current_child + 1 <= p_queue->curr_index &&
+           queue[current_child].priority > queue[current_child + 1].priority)
+            current_child++;
+
+        __exchange_node(&queue[current_parent], &queue[current_child]);
+
+        current_parent = current_child;
+        current_child  = current_parent * 2 + 1;
+    }
+}
+
+void __swim_LastElement_inverted( __PQueue *p_queue )
+{
+    // 1- child -> last node
+    // 2- cmp child, parent where parent -> (child-1)/2
+    // 3- child > parent -> swap and child_index = parent_index
+    // 4- else get out
+    int8_t current_index = p_queue->curr_index;
+    int8_t parent_index;
+    __PQueue_Node* queue = p_queue->queue;
+
+    while (current_index > 0 &&
+           queue[parent_index =  (current_index - 1) / 2].priority > queue[current_index].priority)
+    {
+        __exchange_node(&queue[parent_index], &queue[current_index]);
+        current_index = parent_index;
+    }
+}
+
---
+++ src/inner/inner_priority_queue.h
@@ -0,0 +1,26 @@
+#ifndef INNER_PRIORITY_QUEUE
+#define INNER_PRIORITY_QUEUE
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef struct __PQueue_Node {
+    int8_t priority;
+    void* data;
+} __PQueue_Node;
+
+typedef struct __PQueue {
+    size_t length;
+    long curr_index;
+    __PQueue_Node queue[];
+}__PQueue;
+
+void __exchange_node(__PQueue_Node * node1, __PQueue_Node * node2 );
+void __swim_LastElement( __PQueue *queue );
+void __sink_LastElement( __PQueue *queue );
+void __swim_LastElement_inverted( __PQueue *queue );
+void __sink_LastElement_inverted( __PQueue *queue );
+void* __PQueue_popHead(__PQueue *p_queue);
+
+#endif // INNER_PRIORITY_QUEUE
